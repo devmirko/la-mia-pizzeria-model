5946e8d3b535bd25327c13092c119b8c06e01609
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-11-21 13:48:07 +0100

    update

---
+++ Models/Pizza.cs
@@ -0,0 +1,25 @@
+﻿namespace la_mia_pizzeria_razor_layout.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        public Pizza()
+        {
+
+        }
+
+        public Pizza(string name, string description, string image)
+        {
+            this.Name = Name;
+            this.Description = description;
+            this.Image = image;
+        }
+    }
+}
