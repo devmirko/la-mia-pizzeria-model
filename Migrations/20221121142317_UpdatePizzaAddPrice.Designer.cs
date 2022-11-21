857036921a2a63898c53ac9e8a4b0e2d8931f89a
mirkosimonetti <mirko.simonetti.ta@gmail.com>
2022-11-21 15:54:40 +0100

    update migration

--- Migrations/20221121142317_UpdatePizzaAddPrice.Designer.cs
+++ Migrations/20221121142317_UpdatePizzaAddPrice.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using la_mia_pizzeria_razor_layout.Data;
 
@@ -10,9 +11,11 @@ using la_mia_pizzeria_razor_layout.Data;
 namespace lamiapizzeriarazorlayout.Migrations
 {
     [DbContext(typeof(PizzaDbContext))]
-    partial class PizzaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221121142317_UpdatePizzaAddPrice")]
+    partial class UpdatePizzaAddPrice
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Pizza.cs
+++ Models/Pizza.cs
@@ -1,25 +1,34 @@
-﻿namespace la_mia_pizzeria_razor_layout.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_razor_layout.Models
 {
     public class Pizza
     {
         public int Id { get; set; }
 
+
         public string Name { get; set; }
 
+        [Column(TypeName = "text")]
         public string Description { get; set; }
 
         public string Image { get; set; }
+
+        public double Price { get; set; }
 
         public Pizza()
         {
 
         }
 
-        public Pizza(string name, string description, string image)
+        public Pizza(string name, string description, string image, double price)
         {
-            this.Name = Name;
+            this.Name = name;
             this.Description = description;
             this.Image = image;
+            this.Price = price;
+
         }
     }
 }
